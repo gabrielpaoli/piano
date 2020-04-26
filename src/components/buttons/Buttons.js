ff3731244b79627a90d94f4d6d1418e39db5e9ff
gabrielpaoli <gabrielpaoli@hotmail.com>
2020-04-26 13:33:15 -0300

    add vowels and numbers

--- src/components/buttons/Buttons.js
+++ src/components/buttons/Buttons.js
@@ -3,23 +3,35 @@ import '../../css/Buttons.css';
 import ButtonsLoad from "./ButtonsLoad";
 import animals from "../../json/animals.json";
 import musicalInstruments from "../../json/musicalInstruments.json";
+import vowels from "../../json/vowels.json";
+import numbers from "../../json/numbers.json";
 
 export default function Buttons() {
 	return (
     <div id="container">
+			<ButtonsLoad
+				title={'Animales'}
+				buttons={animals}
+				imageClass={'buttonAnimalSound'}
+			/>
 
-				<ButtonsLoad
-					title={'Animales'}
-					buttons={animals}
-					imageClass={'buttonAnimalSound'}
-				/>
+			<ButtonsLoad
+				title={'Instrumentos musicales'}
+				buttons={musicalInstruments}
+				imageClass={'buttonMusicalInstrumentSound'}
+			/>
 
-				<ButtonsLoad
-					title={'Instrumentos musicales'}
-					buttons={musicalInstruments}
-					imageClass={'buttonMusicalInstrumentSound'}
-				/>
+			<ButtonsLoad
+				title={'Vocales'}
+				buttons={vowels}
+				imageClass={'buttonVowels'}
+			/>
 
+			<ButtonsLoad
+				title={'Numeros'}
+				buttons={numbers}
+				imageClass={'buttonNumbers'}
+			/>
 		</div>
   );
 }
