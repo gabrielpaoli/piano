d4387ab610cc36c3f8d5d0b2b03f72dc976cd139
gabrielpaoli <gabrielpaoli@hotmail.com>
2020-08-04 15:27:25 -0300

    Updates

---
+++ precache-manifest.0d221b8e62a4998125582665c77e41da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3f759ad076bd36a07ec3eec6942b6cf",
+    "url": "/piano/index.html"
+  },
+  {
+    "revision": "31ac4d51229d9fb38484",
+    "url": "/piano/static/css/main.627380f4.chunk.css"
+  },
+  {
+    "revision": "0d972f81ed0ad27e3718",
+    "url": "/piano/static/js/2.0cc78073.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/piano/static/js/2.0cc78073.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31ac4d51229d9fb38484",
+    "url": "/piano/static/js/main.20c37c6f.chunk.js"
+  },
+  {
+    "revision": "3f1131b4868da105c361",
+    "url": "/piano/static/js/runtime-main.2c53cac2.js"
+  }
+]);
